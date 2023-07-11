70140d48d55ba8f5a7e348d12caa0fda9a4b8c92
{iliuhai@aliyun.com} <iliuhai@aliyun.com>
2023-07-12 00:27:49 +0800

    H.LowCode.Docs 支持 markdown 文件渲染

--- docs/H.LowCode.Docs.Dumi/Util/MarkdownToHtml.cs
+++ docs/H.LowCode.Docs.Dumi/Util/MarkdownToHtml.cs
@@ -1,16 +1,38 @@
-﻿using System;
+﻿using Markdig.Extensions.Yaml;
+using Markdig.Renderers;
+using Markdig.Syntax;
+using Markdig;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using System.Reflection;
 
 namespace H.LowCode.Docs.Dumi.Util
 {
     internal class MarkdownToHtml
     {
-        internal static string GeneratorHtml(string markdownFilePath)
+        internal static string ConvertToHtml(string fileName, Type type)
         {
-            return string.Empty;
+            string markdownString = GetEmbeddedFile(fileName, type);
+            var pipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
+            string html = Markdown.ToHtml(markdownString, pipeline);
+            return html;
+        }
+
+        private static string GetEmbeddedFile(string fileName, Type type)
+        {
+            Assembly assembly = Assembly.GetAssembly(type);
+            string resourceName = type.Namespace + "." + fileName;
+
+            assembly.GetManifestResourceStream(resourceName);
+            Stream stream = assembly.GetManifestResourceStream(resourceName);
+            using var streamReader = new StreamReader(stream);
+            var content = streamReader.ReadToEnd();
+
+            return content;
         }
     }
 }
