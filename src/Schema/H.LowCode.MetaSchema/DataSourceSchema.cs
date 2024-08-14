d4512535016b0934caf48f248e35375a46c99408
{iliuhai@aliyun.com} <iliuhai@aliyun.com>
2024-08-15 01:03:42 +0800

    - 将应用管理(页面管理、菜单管理、数据源管理等) 应用强相关功能独立为 H.LowCode.DesignEngine.Admin 项目 - 新增数据源管理相关页面(待实现)

--- src/Schema/H.LowCode.MetaSchema/DataSourceSchema.cs
+++ src/Schema/H.LowCode.MetaSchema/DataSourceSchema.cs
@@ -4,13 +4,37 @@ namespace H.LowCode.MetaSchema
 {
     public class DataSourceSchema
     {
+        public string Id { get; set; }
+
+        public string Key { get; set; }
+
+        [JsonPropertyName("n")]
+        public string Name { get; set; }
+
         [JsonPropertyName("type")]
         public DataSourceEnum DataSourceType { get; set; }
+
+        public string ModifiedUser { get; set; }
+
+        public DateTime ModifiedTime { get; set; }
+
+        public string PublishStatus { get; set; }
+
+        #region 以下属性仅一个有效，根据 DataSourceType 选用
+        [JsonPropertyName("fields")]
+        public List<DataSourceTableFieldSchema> DataSourceTableFields { get; set; } = [];
+
+        [JsonPropertyName("api")]
+        public DataSourceAPISchema DataSourceAPI { get; set; }
+
+        [JsonPropertyName("options")]
+        public List<DataSourceOptionSchema> DataSourceOptions { get; set; } = [];
+        #endregion
 
         /// <summary>
         /// 
         /// </summary>
-        public string Value {  get; set; }
+        public string Value { get; set; }
 
         [JsonPropertyName("vals")]
         public IDictionary<string, string> Values { get; set; }
@@ -20,8 +44,48 @@ namespace H.LowCode.MetaSchema
     {
         Default,
         API,
+        Options,
         SQL,
         Expression, //表达式
         Fiexd  //固定值
+    }
+
+    public class DataSourceTableFieldSchema
+    {
+        public string FieldKey { get; set; }
+
+        public string FieldName { get; set; }
+
+        public string FieldType { get; set; }
+
+        public bool IsPrimaryKey { get; set; }
+
+        public bool IsRequired { get; set; }
+
+        public bool IsUnique { get; set; }
+    }
+
+    public class DataSourceAPISchema
+    {
+        public string BaseUrl { get; set; }
+
+        public string Path { get; set; }
+
+        public string Method { get; set; }
+
+        public IDictionary<string, string> Headers { get; set; }
+
+        public IDictionary<string, string> Queries { get; set; }
+
+        public string Body { get; set; }
+
+        public string Description { get; set; }
+    }
+
+    public class DataSourceOptionSchema
+    {
+        public string OptionKey { get; set; }
+
+        public string OptionName { get; set; }
     }
 }
