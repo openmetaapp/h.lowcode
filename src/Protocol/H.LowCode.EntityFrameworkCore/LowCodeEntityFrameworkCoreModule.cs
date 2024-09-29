4af9944b866898f46b018cdf6526eeafa6093e35
iliuhai <iliuhai@aliyun.com>
2024-09-29 10:24:27 +0800

    新增 H.LowCode.EntityFrameworkCore 用于数据存储（空应用，待实现） 新增 H.LowCode.DbMigrator 用于数据库迁移（空应用，待实现）

---
+++ src/Protocol/H.LowCode.EntityFrameworkCore/LowCodeEntityFrameworkCoreModule.cs
@@ -0,0 +1,11 @@
+﻿using Volo.Abp.Modularity;
+
+namespace H.LowCode.EntityFrameworkCore;
+
+public class LowCodeEntityFrameworkCoreModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+
+    }
+}
