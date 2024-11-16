6e4ad9a181dd5d6df8786ecdfc1800e635b5596b
{iliuhai@aliyun.com} <iliuhai@aliyun.com>
2024-11-16 20:22:13 +0800

    修复新增页面表单页报错; 更新易受攻击包 System.Net.Http 版本

--- src/Protocol/H.LowCode.MetaSchema/Enums/PageTypeEnum.cs
+++ src/Protocol/H.LowCode.MetaSchema/Enums/PageTypeEnum.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,12 @@ namespace H.LowCode.MetaSchema;
 
 public enum PageTypeEnum
 {
+    [Display(Name = "普通")]
     Normal = 0,
+    [Display(Name = "表单")]
     Form = 1,
+    [Display(Name = "列表")]
     Table = 2,
+    [Display(Name = "报表")]
     Report = 5
 }
